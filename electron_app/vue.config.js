e594fee034ae647f10387d0c81e02a087fa1b33d
Divam Gupta <divamgupta@gmail.com>
2022-09-21 00:52:04 -0400

    final changes for 0.2.0

--- electron_app/vue.config.js
+++ electron_app/vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
                 appId: 'com.linerai.liner',
                 afterSign: "./afterSignHook.js",
                 "extraResources": [{
-                    "from": "../stable-diffusion/dist/txt2img",
+                    "from": process.env.BACKEND_BUILD_PATH , 
                     "to": "core",
                     "filter": [
                         "**/*"
@@ -25,7 +25,6 @@ module.exports = {
                     "entitlementsInherit": "build/entitlements.mac.plist",
                     "minimumSystemVersion": "12.5.1",
                     "extendInfo": {
-                        "LSMinimumSystemVersion": "12.5.1"
                     } , 
                     "target": {
                         "target": "dmg",
