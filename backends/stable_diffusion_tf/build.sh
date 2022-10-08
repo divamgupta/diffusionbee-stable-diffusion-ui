2c25175a46a212e5b679445aee05d1203fcbb48a
Divam Gupta <divamgupta@gmail.com>
2022-10-07 22:09:52 -0700

    update build script

--- backends/stable_diffusion_tf/build.sh
+++ backends/stable_diffusion_tf/build.sh
@@ -1,2 +1,6 @@
 pyinstaller   diffusionbee_backend.py  --collect-all tensorflow --collect-all tensorflow-plugins  --noconfirm --clean
 cp stable_diffusion_tf/clip_tokenizer/bpe_simple_vocab_16e6.txt.gz ./dist/diffusionbee_backend
+
+cp -r ../realesrgan_ncnn/models  ./dist/diffusionbee_backend/models 
+cp  ../realesrgan_ncnn/realesrgan_ncnn_macos  ./dist/diffusionbee_backend/realesrgan_ncnn_macos 
+
