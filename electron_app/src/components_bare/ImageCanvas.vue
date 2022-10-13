00c45badc288b98e448603aa06cbfa7f71d9e84d
Divam Gupta <divamgupta@gmail.com>
2022-10-12 23:30:53 -0700

    basic drawing working

--- electron_app/src/components_bare/ImageCanvas.vue
+++ electron_app/src/components_bare/ImageCanvas.vue
@@ -23,19 +23,93 @@ export default {
     },
     components: {},
     mounted() {
-        this.ro = new ResizeObserver(this.on_resize)
-        this.ro.observe(document.getElementById("myCanvas").parentElement)
+        this.ro = new ResizeObserver(this.on_resize);
+        let canvas = document.getElementById("myCanvas");
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.ro.observe(canvas.parentElement);
+        let that = this;
+
+        canvas.addEventListener("mousemove", function (e) {
+            that.findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            that.findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            that.findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            that.findxy('out', e)
+        }, false);
+
     },
     data() {
         return {
             img_tag : undefined,
+            prevX : 0 , 
+            prevY : 0 , 
+            currX : 0 , 
+            currY : 0 , 
+            x : "violet" ,
+            y : 10 ,
+            flag : false,
         };
     },
     methods: {
 
+        findxy(res, e) {
+            if (res == 'down') {
+                this.prevX = this.currX;
+                this.prevY = this.currY;
+                this.currX = e.clientX - this.canvas.parentElement.getBoundingClientRect().x;
+                this.currY = e.clientY - this.canvas.parentElement.getBoundingClientRect().y;
+        
+                this.flag = true;
+                let dot_flag = true;
+                if (dot_flag) {
+                    this.ctx.beginPath();
+                    this.ctx.fillStyle = this.x;
+                    this.ctx.fillRect(this.currX, this.currY, 2, 2);
+                    this.ctx.closePath();
+                    dot_flag = false;
+                }
+            }
+            if (res == 'up' || res == "out") {
+                this.flag = false;
+            }
+            if (res == 'move') {
+                if (this.flag) {
+                    this.prevX = this.currX;
+                    this.prevY = this.currY;
+                    this.currX = e.clientX - this.canvas.parentElement.getBoundingClientRect().x;
+                    this.currY = e.clientY - this.canvas.parentElement.getBoundingClientRect().y;
+                    this.draw();
+
+                    console.log(e.clientX - this.canvas.parentElement.getBoundingClientRect().x)
+                }
+            }
+        },
+
+        draw() {
+            let xmul =  this.canvas.width / this.canvas.offsetWidth;
+            let ymul = this.canvas.height / this.canvas.offsetHeight;
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.prevX*xmul, this.prevY*ymul  );
+            this.ctx.lineTo(this.currX*xmul, this.currY*ymul );
+            this.ctx.strokeStyle = this.x;
+            this.ctx.lineWidth = this.y * Math.sqrt(xmul * ymul);
+            this.ctx.stroke();
+            this.ctx.closePath();
+        },
+
         on_resize(){
             let canvas = document.getElementById("myCanvas");
             let img = this.img_tag;
+
+            if(!img)
+                return;
+            
             let ph = canvas.parentElement.offsetHeight;
             let pw = canvas.parentElement.offsetWidth;
 
@@ -54,11 +128,12 @@ export default {
         on_img_change(){
             let that = this;
             addImageProcess('file://'+this.image_source).then(function(img_tag){
+
                 that.img_tag = img_tag;
                 let canvas = document.getElementById("myCanvas");
                 let img = that.img_tag;
                 let ctx = canvas.getContext("2d");
-
+                
                 that.on_resize()
 
                 canvas.height = img.height;
