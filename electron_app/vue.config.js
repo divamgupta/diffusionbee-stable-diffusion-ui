e23b2cb2d3ff1b91e56203ad9c74d5dabf54ed1e
joshJarr <josh.jarvis@potatolondon.com>
2022-11-07 15:22:15 +0000

    fix: Add `NodePolyfill` plugin to webpack after upgrading to webpack 5+.

--- electron_app/vue.config.js
+++ electron_app/vue.config.js
@@ -1,17 +1,22 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
-    
+    configureWebpack: {
+        plugins: [
+            new NodePolyfillPlugin()
+        ]
+    },
     pluginOptions: {
         electronBuilder: {
             preload: './src/preload.js',
-            
+
             // Or, for multiple preload files:
             // preload: { preload: 'src/preload.js', otherPreload: 'src/preload2.js' }
             builderOptions: {
                 appId: 'com.linerai.liner',
                 afterSign: "./afterSignHook.js",
                 "extraResources": [{
-                    "from": process.env.BACKEND_BUILD_PATH , 
+                    "from": process.env.BACKEND_BUILD_PATH ,
                     "to": "core",
                     "filter": [
                         "**/*"
@@ -19,15 +24,15 @@ module.exports = {
                 }], // access via path.join(path.dirname(__dirname), 'liner_core' );
 
                 "mac": {
-                    "icon" : "build/Icon-1024.png" , 
+                    "icon" : "build/Icon-1024.png" ,
                     "hardenedRuntime": true,
                     "entitlements": "build/entitlements.mac.plist",
                     "entitlementsInherit": "build/entitlements.mac.plist",
                     "minimumSystemVersion": "12.3.0",
                     "extendInfo": {
                         "LSMinimumSystemVersion": "12.3.0"
-                    } , 
-                    
+                    } ,
+
                     "target": {
                         "target": "dmg",
                         "arch": [
@@ -37,11 +42,11 @@ module.exports = {
                 },
 
                 "win": {
-                    "icon" : "build/Icon-1024.png" , 
+                    "icon" : "build/Icon-1024.png" ,
                     "target": {
                         "target": "NSIS",
                         "arch": [
-                            process.arch   
+                            process.arch
                         ]
                     }
                 }
