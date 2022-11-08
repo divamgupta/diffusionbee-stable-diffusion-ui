32e27d2cae61461b760a72571fd75e1cbf149055
joshJarr <josh.jarvis@potatolondon.com>
2022-11-08 11:18:52 +0000

    chore: revert whitespace changes for a clean commit log.

--- electron_app/vue.config.js
+++ electron_app/vue.config.js
@@ -1,6 +1,7 @@
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
+    
     configureWebpack: {
         plugins: [
             new NodePolyfillPlugin()
@@ -9,14 +10,14 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             preload: './src/preload.js',
-
+            
             // Or, for multiple preload files:
             // preload: { preload: 'src/preload.js', otherPreload: 'src/preload2.js' }
             builderOptions: {
                 appId: 'com.linerai.liner',
                 afterSign: "./afterSignHook.js",
                 "extraResources": [{
-                    "from": process.env.BACKEND_BUILD_PATH ,
+                    "from": process.env.BACKEND_BUILD_PATH , 
                     "to": "core",
                     "filter": [
                         "**/*"
@@ -24,15 +25,15 @@ module.exports = {
                 }], // access via path.join(path.dirname(__dirname), 'liner_core' );
 
                 "mac": {
-                    "icon" : "build/Icon-1024.png" ,
+                    "icon" : "build/Icon-1024.png" , 
                     "hardenedRuntime": true,
                     "entitlements": "build/entitlements.mac.plist",
                     "entitlementsInherit": "build/entitlements.mac.plist",
                     "minimumSystemVersion": "12.3.0",
                     "extendInfo": {
                         "LSMinimumSystemVersion": "12.3.0"
-                    } ,
-
+                    } , 
+                    
                     "target": {
                         "target": "dmg",
                         "arch": [
@@ -42,11 +43,11 @@ module.exports = {
                 },
 
                 "win": {
-                    "icon" : "build/Icon-1024.png" ,
+                    "icon" : "build/Icon-1024.png" , 
                     "target": {
                         "target": "NSIS",
                         "arch": [
-                            process.arch
+                            process.arch   
                         ]
                     }
                 }
